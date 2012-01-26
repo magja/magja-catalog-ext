e33497b47b893fbfd24acac360969076293db789
Agus Prasetyo <agus@bippo.co.id>
2012-01-26 16:06:25 +0700

    CLOSED - task 279: 05. Integrasi Atribut Set : Magento > Openbravo [Agus] http://mahameru.unfuddle.com/projects/1/tickets/by_number/279 Signed-off-by: Agus Prasetyo <agus@bippo.co.id>

---
+++ app/code/community/Magja/Catalog/Model/CatalogInventory/Api.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_CatalogInventory
+ * @copyright   Copyright (c) 2011 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Catalog inventory api
+ *
+ * @category   Mage
+ * @package    Mage_CatalogInventory
+ * @author     Magento Core Team <core@magentocommerce.com>
+ */
+class Magja_Catalog_Model_CatalogInventory_Api extends Mage_CatalogInventory_Model_Stock_Item_Api
+{
+	
+    public function items($productIds)
+    {
+        if (!is_array($productIds)) {
+        	
+        	if (is_string($productIds)) {
+        		$productIds = split(',', $productIds);
+        	} else {
+            	$productIds = array($productIds);
+        	}
+        }
+
+        $product = Mage::getModel('catalog/product');
+
+        foreach ($productIds as &$productId) {
+            if ($newId = $product->getIdBySku($productId)) {
+                $productId = $newId;
+            }
+        }
+
+        $collection = Mage::getModel('catalog/product')
+            ->getCollection()
+            ->setFlag('require_stock_items', true)
+            ->addFieldToFilter('entity_id', array('in'=>$productIds));
+
+        $result = array();
+
+        foreach ($collection as $product) {
+            if ($product->getStockItem()) {
+                $result[] = array(
+                    'product_id'    => $product->getId(),
+                    'sku'           => $product->getSku(),
+                    'qty'           => $product->getStockItem()->getQty(),
+                    'is_in_stock'   => $product->getStockItem()->getIsInStock()
+                );
+            }
+        }
+
+        return $result;
+    }
+} // Class Mage_CatalogInventory_Model_Stock_Item_Api End
