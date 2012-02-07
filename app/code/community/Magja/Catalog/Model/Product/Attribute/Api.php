8ddfe5e8e5bde51de32a5e2b99fad9a71983b30e
Agus Prasetyo <agus@bippo.co.id>
2012-02-07 14:13:33 +0700

    NEW - task 265: 02.C. Migrasi Produk > Magento [Agus] http://mahameru.unfuddle.com/projects/1/tickets/by_number/265 Signed-off-by: Agus Prasetyo <agus@bippo.co.id>

--- app/code/community/Magja/Catalog/Model/Product/Attribute/Api.php
+++ app/code/community/Magja/Catalog/Model/Product/Attribute/Api.php
@@ -9,80 +9,20 @@
  */
 class Magja_Catalog_Model_Product_Attribute_Api extends Mage_Catalog_Model_Product_Attribute_Api {
 	
-	/**
-	 * Retrieve product attribute info by code
-	 *
-	 * @param string $attributeCode
-	 * @param string|int $store
-	 * @return array
-	 */
-	public function info($attributeCode, $store = null) {
-		
-		$storeId = $this->_getStoreId ( $store );
-		$attribute = Mage::getModel ( 'catalog/product' )->setStoreId ( $storeId )->getResource ()->getAttribute ( $attributeCode )->setStoreId ( $storeId );
-		
-		if (!$attribute) {
-            $this->_fault('not_exists');
-        }
-		
-		if (! $attribute->getId () || $attribute->isScopeGlobal ()) {
-			$scope = 'global';
-		} elseif ($attribute->isScopeWebsite ()) {
-			$scope = 'website';
-		} else {
-			$scope = 'store';
-		}
-		
-		//$result = print_r($attribute, true);
-		
-        $result = array(
-        	'attribute_id' 	=> $attribute->getId(),
-            'code'        	=> $attribute->getAttributeCode(),
-            'type'       	=> $attribute->getFrontendInput(),
-            'backend' 		=> $attribute->getData('backend_type'),
-            'frontend'   	=> $attribute->getFrontendLabel(),
-        	'label'   		=> $attribute->getFrontendLabel(),
-        	'class'   		=> $attribute->getData('frontend_class'),
-        	'default'   	=> $attribute->getData('default_value'),
-        	'visible'   	=> $attribute->getIsVisible(),
-        	'required'   	=> $attribute->getData('is_required'),
-        	'user_defined'  => $attribute->getData('is_user_defined'),
-        	'searchable'   	=> $attribute->getIsSearchable(),
-        	'filterable'   	=> $attribute->getIsFilterable(),
-        	'comparable'   	=> $attribute->getIsComparable(),
-        	'visible_on_front' => $attribute->getData('is_visible_on_front'),
-        	'visible_in_advanced_search' => $attribute->getIsVisibleInAdvancedSearch(),
-        	'unique'		=> $attribute->getData('is_unique'),
-        	'used_for_sort_by' => $attribute->getUsedForSortBy(),
-        	'sortBy' 		=> $attribute->getUsedForSortBy(),
-        	'scope'        	=> $scope,
-        	'is_configurable' => $attribute->getIsConfigurable(),
-        );
-        
-		return $result;
-	}
-	
-	/**
-	 * Create new product attribute.
-	 *
-	 * @param string $attributeName
-	 * @param array $attributeData
-	 * @param string|int $store
-	 * @return int
-	 */
-	public function create($attributeData, $store = null) {
-		$attributeName = $attributeDate['attribute_code'];
-		
-		// create product attribute
-		$installer = new Mage_Catalog_Model_Resource_Eav_Mysql4_Setup ( 'core_setup' );
-		$installer->addAttribute ( 'catalog_product', $attributeName, $attributeData );
-		
-		// get product attribute id
-		$storeId = $this->_getStoreId ( $store );
-		$attribute = Mage::getModel ( 'catalog/product' )->setStoreId ( $storeId )->getResource ()->getAttribute ( $attributeName );
-		
-		return $attribute->getId ();
-	}
+    /**
+     * Create new product attribute
+     *
+     * @param array $data input data
+     * @return integer
+     */
+    public function create($data) {
+    	// if frontend_label is not provided as an array, help the client
+    	if (!empty($data['frontend_label']) && !is_array($data['frontend_label'])) {
+    		$frontend_label = $data['frontend_label']; 
+    		$data['frontend_label'] = array( array('store_id' => 0, 'label' => $frontend_label) );
+    	}
+    	return parent::create($data);
+    }
 	
 	/**
 	 * Create attribute options
