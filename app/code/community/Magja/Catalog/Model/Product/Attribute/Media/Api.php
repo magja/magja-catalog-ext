08726dd927fa221cc18ac0562af382071382a802
Agus Prasetyo <agus@bippo.co.id>
2012-02-17 17:24:49 +0700

    REASSIGNED - task 265: 02.C. Migrasi Produk > Magento [Agus] http://mahameru.unfuddle.com/projects/1/tickets/by_number/265 Signed-off-by: Agus Prasetyo <agus@bippo.co.id>
    refs #265 Upload gambar

--- app/code/community/Magja/Catalog/Model/Product/Attribute/Media/Api.php
+++ app/code/community/Magja/Catalog/Model/Product/Attribute/Media/Api.php
@@ -18,6 +18,35 @@ class Magja_Catalog_Model_Product_Attribute_Media_Api extends Mage_Catalog_Model
 	public function md5($file) {
 		return md5_file ( Mage::getBaseDir ( 'media' ) . DS . 'catalog' . DS . 'product' . $file );
 	}
+	
+	/**
+	* Download image from a URL, add it as product image and return image filename
+	*
+	* @param int|string $productId
+	* @param string $name Image filename
+	* @param string $mimetype Image MIME type
+	* @param string $url Image URL
+	* @param string|int $store
+	* @return string
+	*/
+	public function createFromUrl($productId, $name, $mimetype, $url, $label, $types = array('image', 'small_image', 'thumbnail'),
+		$store = null, $identifierType = null) {
+		if (is_string($types))
+			$types = explode(',', $types);
+		Mage::log("Downloading from ". $url);
+		$content = file_get_contents($url);
+		$data = array(
+			'label' => $label,
+			'types' => $types,
+			'exclude' => 0,
+			'file' => array(
+				'name' => $name,
+				'content' => base64_encode($content),
+				'mime' => $mimetype
+			));
+		return $this->create($productId, $data, $store, $identifierType);
+	}
+	
 }
 
 ?>
